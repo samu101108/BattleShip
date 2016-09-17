624e9cbdf59a62bdb3bad861f2d6200f9fc04fb7
Samuel Barbosa <samu101108@gmail.com>
2016-09-17 11:59:16 +0000

    Some game logic added

--- battleShip.py
+++ battleShip.py
@@ -1,4 +1,4 @@
-from random import randint 
+from random import randint
 
 board = []
 
@@ -9,13 +9,31 @@ def print_board(board):
     for row in board:
         print " ".join(row)
 
-# Add your code below!
+print_board(board)
+
 def random_row(board):
-    return randint(0,len(board) -1)
+    return randint(0, len(board) - 1)
+
 def random_col(board):
-    return randint(0,len(board) -1)
-    
-random_row(board)
-random_col(board)
-guess_row = int(raw_input("Guess Row: "))
-guess_col = int(raw_input("Guess Col: "))
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+guess_row = int(raw_input("Guess Row:"))
+guess_col = int(raw_input("Guess Col:"))
+
+print ship_row
+print ship_col
+
+# Write your code below!
+if guess_row == ship_row and guess_col == ship_col:
+    print "Congratulations! You sank my battleship"
+else:
+    if guess_row not in range(5) or guess_col not in range(5):
+        print "Oops, that's not even in the ocean."
+    elif board[guess_row][guess_col]== "X":
+	print "You guessed that one already."
+    else:
+        print "You missed my battleship!"
+        board[guess_row][guess_col] = "X"
+        print_board(board)
